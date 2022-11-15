2da043ccb439da0142c0ce783929e9f93202bf10
Lucas David Cardozo <lucas.david.car99@gmail.com>
2022-11-15 17:05:50 -0300

    Configuracion inicial del proyecto

---
+++ src/main/java/weatherApp/delivery/HomeController.java
@@ -0,0 +1,13 @@
+package weatherApp.delivery;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @RequestMapping(path = "/")
+    public ModelAndView goHome(){
+        return new ModelAndView("home");
+    }
+}
