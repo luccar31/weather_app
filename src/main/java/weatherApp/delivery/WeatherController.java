fe93c4ec9950c0c27697c06a31a024baae36e1ea
Lucas David Cardozo <lucas.david.car99@gmail.com>
2022-11-16 15:55:15 -0300

    Añadido timer para consultar y persistir api

--- src/main/java/weatherApp/delivery/WeatherController.java
+++ src/main/java/weatherApp/delivery/WeatherController.java
@@ -1,37 +1,55 @@
 package weatherApp.delivery;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-import weatherApp.domain.Location.WeatherLocation;
+import weatherApp.domain.Weather.WeatherLocation;
 import weatherApp.domain.Weather.WeatherService;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Controller
 public class WeatherController {
 
     private WeatherService weatherService;
+    private Boolean activated = false;
 
     @Autowired
     public WeatherController(WeatherService weatherService){
         this.weatherService = weatherService;
     }
 
+    @RequestMapping(path = "/")
+    public ModelAndView goHome(){
+
+        //TODO: extract to outside method
+        if(!activated){
+            TimerTask timerTask = new TimerTask() {
+                public void run() {
+                    weatherService.scheduledTask("https://ws.smn.gob.ar/map_items/weather");
+                }
+            };
+
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(timerTask, 0, 1000 * 60 * 5);
+            activated = true;
+        }
+
+        return new ModelAndView("home");
+    }
+
     @RequestMapping(path = "/weather", method = RequestMethod.POST)
     public ModelAndView showWeather(){
         ModelMap model = new ModelMap();
 
-        //TODO: implementar tests de esto a ver si es lo que se necesita
         List<WeatherLocation> weatherLocationList = weatherService.getAllLocations();
 
-        model.put("locationList", weatherLocationList);
+        model.put("weatherList", weatherLocationList);
 
         return new ModelAndView("weather", model);
     }
